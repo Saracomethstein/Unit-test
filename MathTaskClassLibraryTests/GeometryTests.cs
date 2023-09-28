0020450224b8f346e9a1adb676706dbcc12018a1
Saracomethstein <adel.sevastianov@yandex.ru>
2023-09-28 12:51:19 +0300

    Finish first task and unit-test

---
+++ MathTaskClassLibraryTests/GeometryTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MathTaskClassLibrary;
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15returned()
+        {
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+            Geometry geometry = new Geometry();
+            int actual = geometry.RectangleArea(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
